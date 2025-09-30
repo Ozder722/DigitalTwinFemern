8b96885a027ae43995a458e2b88c7899b3dc640f
Ozder722 <carlosozdk@gmail.com>
2025-09-30 09:08:23 +0200

    Event system set up

---
+++ Assets/EventSystem_Tunnel.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelEventSystem : MonoBehaviour
+{
+    [SerializeField] List<Tunnel> elementer = new List<Tunnel>();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TriggerRandomEvent();
+        }
+    }
+
+    private void TriggerRandomEvent()
+    {
+        if (elementer == null || elementer.Count == 0) return;
+
+        // 1. Vælg tilfældig tunnel
+        Tunnel chosenTunnel = elementer[Random.Range(0, elementer.Count)];
+        if (chosenTunnel == null) return;
+
+        // 2. Vælg om vi tager vent eller light
+        bool pickVent = (Random.Range(0, 2) == 0);
+        ItemStatus[] arrayToPick = pickVent ? chosenTunnel.vent : chosenTunnel.GetComponent<Light>();
+        if (arrayToPick == null || arrayToPick.Length == 0) return;
+
+        // 3. Vælg tilfældigt objekt i arrayet
+        ItemStatus chosenItem = arrayToPick[Random.Range(0, arrayToPick.Length)];
+        if (chosenItem == null) return;
+
+        // 4. Vælg en tilfældig fejl (gul, rød eller blå)
+        ItemStatus.errorColor randomError = GetRandomErrorColor();
+
+        // 5. Trigger state på objektet
+        chosenItem.SimulateState(randomError);
+
+        Debug.Log($"Tunnel event: {chosenTunnel.name} - {chosenItem.name} -> {randomError}");
+    }
+
+    private ItemStatus.errorColor GetRandomErrorColor()
+    {
+        int r = Random.Range(0, 3); // 0,1,2
+        switch (r)
+        {
+            case 0: return ItemStatus.errorColor.yellow;
+            case 1: return ItemStatus.errorColor.red;
+            case 2: return ItemStatus.errorColor.blue;
+            default: return ItemStatus.errorColor.red;
+        }
+    }
+}
--- Assets/TestItemEvents.cs
+++ Assets/TestItemEvents.cs
@@ -1,22 +1,22 @@
-//using NUnit.Framework.Internal;
-//using System;
-//using UnityEngine;
-//using static ItemEvent;
+using NUnit.Framework.Internal;
+using System;
+using UnityEngine;
+using static ItemEvent;
 
-//public class ItemEventSubscriber : MonoBehaviour
-//{
-//    // Start is called once before the first execution of Update after the MonoBehaviour is created
-//    void Start()
-//    {
-//       ItemStatus ItemStatus = GetComponent<ItemStatus>();
+public class ItemEventSubscriber : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ItemStatus ItemStatus = GetComponent<ItemStatus>();
 
-//        ItemStatus.OnKeyPressed += Test_OnSpacePressed;
-//    }
+        //ItemStatus.OnKeyPressed += Test_OnSpacePressed;
+    }
 
-//    private void Test_OnSpacePressed(object sender, ItemEvent.OnKeyPressedEventArgs e)
-//    {
-//        Debug.Log("Space! " + e.erorColor);
-        
-//    }
+    private void Test_OnSpacePressed(object sender, ItemEvent.OnKeyPressedEventArgs e)
+    {
+        Debug.Log("Space! ");
 
-//}
+    }
+
+}
